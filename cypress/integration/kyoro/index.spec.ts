f320ba47c9cad73ff658e544dd22fc6f3f4f023d
James <james.waugh.r@gmail.com>
2020-08-29 08:14:16 -0400

    First Kyoro test doing something reasonable

--- cypress/integration/kyoro/index.spec.ts
+++ cypress/integration/kyoro/index.spec.ts
@@ -1,5 +1,9 @@
-describe("My First Test", () => {
-  it("Does not do much!", () => {
-    expect(true).to.equal(true);
+beforeEach(() => {
+  cy.visit("http://localhost:5000");
+});
+
+describe("kyoro index page", () => {
+  it("contains the website header", () => {
+    cy.get("main").children("h1").first().should("have.text", "Hello Kyoro!");
   });
 });
