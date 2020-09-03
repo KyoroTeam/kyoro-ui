644aeb27fa62df3b69f3df5509224159bf4d7c90
James <james.waugh.r@gmail.com>
2020-09-02 22:16:47 -0400

    Set up ad-hoc dev install script for quick adding testing. Format files

--- addon/__init__.py
+++ addon/__init__.py
@@ -15,98 +15,21 @@ addon_path = os.path.dirname(__file__)
 class KyroWebView(AnkiWebView):
     def __init__(self):
         AnkiWebView.__init__(self, title="Kyoro")
-        with open(os.path.join(addon_path, "app.js"), "r") as f:
+
+        with open(os.path.join(addon_path, "bundle.js"), "r") as f:
             appContents = f.read()
-        style = """
-html, body {
-    position: relative;
-    width: 100%;
-    height: 100%;
-}
-
-body {
-    color: #333;
-    margin: 0;
-    padding: 8px;
-    box-sizing: border-box;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
-}
-
-a {
-    color: rgb(0,100,200);
-    text-decoration: none;
-}
-
-a:hover {
-    text-decoration: underline;
-}
-
-a:visited {
-    color: rgb(0,80,160);
-}
-
-label {
-    display: block;
-}
-
-input, button, select, textarea {
-    font-family: inherit;
-    font-size: inherit;
-    -webkit-padding: 0.4em 0;
-    padding: 0.4em;
-    margin: 0 0 0.5em 0;
-    box-sizing: border-box;
-    border: 1px solid #ccc;
-    border-radius: 2px;
-}
-
-input:disabled {
-    color: #ccc;
-}
-
-button {
-    color: #333;
-    background-color: #f4f4f4;
-    outline: none;
-}
-
-button:disabled {
-    color: #999;
-}
-
-button:not(:disabled):active {
-    background-color: #ddd;
-}
-
-button:focus {
-    border-color: #666;
-}
-"""
+        with open(os.path.join(addon_path, "global.css"), "r") as f:
+            globalCss = f.read()
+        with open(os.path.join(addon_path, "bundle.css"), "r") as f:
+            appCss = f.read()
 
         html = """
-<script defer>
-{0}
-</script>
-<style>
-{1}
-</style>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset='utf-8'>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
+            <script defer>{0}</script>
+            <style>{1}</style>
+            <style>{2}</style>
+            <!DOCTYPE html><body></body></html>
+        """.format(appContents, globalCss, appCss)
 
-    <title>Svelte app</title>
-
-    <link rel='icon' type='image/png' href='/favicon.png'>
-    <link rel='stylesheet' href='/global.css'>
-    <link rel='stylesheet' href='/build/bundle.css'>
-</head>
-
-<body>
-</body>
-</html>
-        """.format(appContents, style)
         self.stdHtml(html)
 
 
