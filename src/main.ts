4c8144bf116745c69e8e0fb27f047fb848bbc146
James <james.waugh.r@gmail.com>
2020-10-03 15:20:08 -0400

    Add Carbon

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-import './utils.css';
 import App from './App.svelte';
 
-const app = new App({
-    target: document.body,
-});
+const app = new App({ target: document.body });
 
 export default app;
