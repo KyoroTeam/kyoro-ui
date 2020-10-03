25ee24d4f0f7ca12bdc50b46710a0a8a94143f37
James <james.waugh.r@gmail.com>
2020-10-03 14:54:00 -0400

    Template working with TS main app

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Nav from './components/nav.svelte';
+    import Button from './components/Button.svelte';
+    let menuItems = [
+        { link: '#', name: 'Docs' },
+        { link: '#', name: 'Home' },
+        { link: '#', name: 'Blog' },
+    ];
+    const a: string = 'string';
+</script>
+
+<Nav {menuItems} />
+<Button text="Something" />
