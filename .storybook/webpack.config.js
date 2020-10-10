03566783635412ce2632fee467645c8a50a652d3
James <james.waugh.r@gmail.com>
2020-10-10 06:13:11 -0400

    Removing compilation errors and formatting

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,35 +2,31 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = ({ config, mode }) => {
-    const svelteLoader = config.module.rules.find(
-        r => r.loader && r.loader.includes('svelte-loader'),
-    );
+  const svelteLoader = config.module.rules.find(r => r.loader && r.loader.includes('svelte-loader'));
 
-    console.log(config);
+  svelteLoader.options = {
+    ...svelteLoader.options,
+    emitCss: true,
+    hotReload: false,
+    preprocess: require('svelte-preprocess')({}),
+  };
 
-    svelteLoader.options = {
-        ...svelteLoader.options,
-        emitCss: true,
-        hotReload: false,
-        preprocess: require('svelte-preprocess')({}),
-    };
+  config.module.rules.push({
+    test: /\.css$/,
+    loaders: [
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true,
+          config: {
+            path: './.storybook/',
+          },
+        },
+      },
+    ],
 
-    config.module.rules.push({
-        test: /\.css$/,
-        loaders: [
-            {
-                loader: 'postcss-loader',
-                options: {
-                    sourceMap: true,
-                    config: {
-                        path: './.storybook/',
-                    },
-                },
-            },
-        ],
+    include: path.resolve(__dirname, '../storybook/'),
+  });
 
-        include: path.resolve(__dirname, '../storybook/'),
-    });
-
-    return config;
+  return config;
 };
--- storybook/stories/carbon/carbon.stories.js
+++ storybook/stories/carbon/carbon.stories.js
@@ -2,17 +2,13 @@ import '../../css/utils.css';
 
 import { storiesOf } from '@storybook/svelte';
 import { action } from '@storybook/addon-actions';
-import ButtonSimple from '../../components/buttons/ButtonSimple.svelte';
-import App from "../../../src/App.svelte";
+import App from '../../../src/App.svelte';
 
 storiesOf('Carbon | Carbon', module)
-    //Simple Button
-    .add(
-        'Simple',
-        () => ({
-            Component: App,
-            on: {
-                customEventHandler: action('This custom event handler works'),
-            },
-        }),
-    )
+  // Carbon Sample
+  .add('Simple', () => ({
+    Component: App,
+    on: {
+      customEventHandler: action('This custom event handler works'),
+    },
+  }));
