144443fa9b62a657b5277fd4619bebfe88e8e80f
James <james.waugh.r@gmail.com>
2020-11-12 22:15:45 -0500

    Splitting out main page into components

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -7,53 +7,15 @@
     Tabs,
     TabContent,
     Tab,
-    Select,
-    SelectItem,
-    Table,
-    TableHeader,
-    TableRow,
-    TableCell,
-    TableHead,
-    TableBody,
-    Search,
-    Button,
-    DataTable,
-    MultiSelect,
-    Tag,
-    Checkbox,
-    Pagination,
-    ButtonSet,
   } from "carbon-components-svelte";
   import AnkiConectGate from "./components/AnkiConectGate.svelte";
   import Header from "./components/Header.svelte";
   import Theme from "./components/Theme.svelte";
-  import AnkiConnectProvider from "./services/AnkiConnectProvider.svelte";
-  import DeckSelect from "./components/DeckSelect.svelte";
-  import Add16 from "carbon-icons-svelte/lib/Add16";
-  import MappingsCreator from "./components/MappingsCreator.svelte";
-  let theme: "g10" = "g10";
 
-  const rows = [
-    {
-      id: "load-balancer-1",
-      name: "Load Balancer 1",
-      rule: "Round robin",
-      Status: "Starting",
-    },
-    {
-      id: "load-balancer-2",
-      name: "Load Balancer 2",
-      rule: "DNS delegation",
-      status: "Active",
-    },
-    {
-      id: "load-balancer-3",
-      name: "Load Balancer 3",
-      rule: "Round robin",
-      status: "Disabled",
-    },
-  ];
-  const headers = ["Use", "Sentence", "Source", "Tags"];
+  import AnkiConnectProvider from "./services/AnkiConnectProvider.svelte";
+  import MappingsCreator from "./components/MappingsCreator.svelte";
+  import SeachTab from "./components/SeachTab.svelte";
+  let theme: "g10" = "g10";
 </script>
 
 <Theme bind:theme>
@@ -71,82 +33,8 @@
                 <div slot="content" class="tabbed-content">
                   <Grid as fullWidth>
                     <TabContent>
-                      <Row>
-                        <Column lg={8}>
-                          <ButtonSet>
-                            <Search placeholder="Enter a phrase..." />
-                            <Button>Search</Button>
-                          </ButtonSet>
-                        </Column>
-                        <Column lg={4}>
-                          <MultiSelect
-                            size="xl"
-                            label="Sentence Sources"
-                            items={[{ id: '0', text: 'Slack' }, { id: '1', text: 'Email' }, { id: '2', text: 'Fax' }]}
-                            selectedIds={['0', '1']} />
-                        </Column>
-                      </Row>
-                      <Row>
-                        <Column md={4} lg={12}>
-                          <DataTable>
-                            <Table>
-                              <TableHead>
-                                <TableRow>
-                                  {#each headers as header}
-                                    <TableHeader>{header}</TableHeader>
-                                  {/each}
-                                </TableRow>
-                              </TableHead>
-                              <TableBody>
-                                {#each rows as _}
-                                  <TableRow
-                                    on:click={() => console.log('nice')}>
-                                    <TableCell>
-                                      <Checkbox />
-                                    </TableCell>
-                                    <TableCell>
-                                      <Column noGutterLeft>
-                                        <p class="border-gray-400">
-                                          I could not persuade him that it was
-                                          true.
-                                        </p>
-                                        <p>
-                                          それが本当だと彼に<b>納得</b>させることができなかった。
-                                        </p>
-                                      </Column>
-                                    </TableCell>
-                                    <TableCell>Tatoeba</TableCell>
-                                    <TableCell>
-                                      <Tag>List 907</Tag>
-                                      <Tag>male name</Tag>
-                                    </TableCell>
-                                    <!-- {#each Object.keys(row).filter(key => key !== 'id') as key}
-									<TableCell>{row[key]}</TableCell>
-								  {/each} -->
-                                  </TableRow>
-                                {/each}
-                              </TableBody>
-                            </Table>
-                          </DataTable>
-                          <Pagination />
-                        </Column>
-                      </Row>
-                      <Row>
-                        <Column lg={6}>
-                          <ButtonSet>
-                            <DeckSelect />
-                            <Select labelText="Target Note" selected="g10">
-                              <SelectItem value="white" text="White" />
-                              <SelectItem value="g10" text="Gray 10" />
-                              <SelectItem value="g90" text="Gray 90" />
-                              <SelectItem value="g100" text="Gray 100" />
-                            </Select>
-                          </ButtonSet>
-                        </Column>
-                        <Column lg={{ span: 2, offset: 4 }}>
-                          <Button icon={Add16}>Add Selected</Button>
-                        </Column>
-                      </Row>
+                      Hello
+                      <SeachTab />
                     </TabContent>
                     <TabContent>
                       <Row />
---
+++ app/src/components/SeachTab.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+  import {
+    Row,
+    Column,
+    Select,
+    SelectItem,
+    Table,
+    TableHeader,
+    TableRow,
+    TableCell,
+    TableHead,
+    TableBody,
+    Search,
+    Button,
+    DataTable,
+    MultiSelect,
+    Tag,
+    Checkbox,
+    Pagination,
+    ButtonSet,
+  } from "carbon-components-svelte";
+  import Add16 from "carbon-icons-svelte/lib/Add16";
+  import DeckSelect from "./DeckSelect.svelte";
+  const rows = [
+    {
+      id: "load-balancer-1",
+      name: "Load Balancer 1",
+      rule: "Round robin",
+      Status: "Starting",
+    },
+    {
+      id: "load-balancer-2",
+      name: "Load Balancer 2",
+      rule: "DNS delegation",
+      status: "Active",
+    },
+    {
+      id: "load-balancer-3",
+      name: "Load Balancer 3",
+      rule: "Round robin",
+      status: "Disabled",
+    },
+  ];
+  const headers = ["Use", "Sentence", "Source", "Tags"];
+</script>
+
+<div>
+  <Row>
+    <Column>
+      <ButtonSet>
+        <Search placeholder="Enter a phrase..." />
+        <Button>Search</Button>
+      </ButtonSet>
+    </Column>
+    <Column>
+      <MultiSelect
+        size="xl"
+        label="Sentence Sources"
+        items={[{ id: '0', text: 'Slack' }, { id: '1', text: 'Email' }, { id: '2', text: 'Fax' }]}
+        selectedIds={['0', '1']} />
+    </Column>
+  </Row>
+  <Row>
+    <Column>
+      <DataTable>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {#each headers as header}
+                <TableHeader>{header}</TableHeader>
+              {/each}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {#each rows as _}
+              <TableRow on:click={() => console.log('nice')}>
+                <TableCell>
+                  <Checkbox />
+                </TableCell>
+                <TableCell>
+                  <Column noGutterLeft>
+                    <p class="border-gray-400">
+                      I could not persuade him that it was true.
+                    </p>
+                    <p>
+                      それが本当だと彼に<b>納得</b>させることができなかった。
+                    </p>
+                  </Column>
+                </TableCell>
+                <TableCell>Tatoeba</TableCell>
+                <TableCell>
+                  <Tag>List 907</Tag>
+                  <Tag>male name</Tag>
+                </TableCell>
+                <!-- {#each Object.keys(row).filter(key => key !== 'id') as key}
+                    <TableCell>{row[key]}</TableCell>
+                    {/each} -->
+              </TableRow>
+            {/each}
+          </TableBody>
+        </Table>
+      </DataTable>
+      <Pagination />
+    </Column>
+  </Row>
+  <Row>
+    <Column>
+      <ButtonSet>
+        <DeckSelect />
+        <Select labelText="Target Note" selected="g10">
+          <SelectItem value="white" text="White" />
+          <SelectItem value="g10" text="Gray 10" />
+          <SelectItem value="g90" text="Gray 90" />
+          <SelectItem value="g100" text="Gray 100" />
+        </Select>
+      </ButtonSet>
+    </Column>
+    <Column lg={{ span: 2, offset: 4 }}>
+      <Button icon={Add16}>Add Selected</Button>
+    </Column>
+  </Row>
+</div>
