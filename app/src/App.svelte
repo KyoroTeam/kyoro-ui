51f90985ba5533ed24a626f2095baee73a6af20e
James <james.waugh.r@gmail.com>
2021-04-17 22:30:55 -0400

    [#2] Start putting app into a better UI shell to support multiple funcitons

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -6,48 +6,79 @@
     Column,
     Tabs,
     TabContent,
+    Header,
     Tab,
+    SideNav,
+    SideNavItems,
+    SideNavLink,
+    HeaderNav,
+    HeaderNavItem,
+    HeaderNavMenu,
+    SideNavDivider,
   } from "carbon-components-svelte";
   import AnkiConectGate from "./components/AnkiConectGate.svelte";
-  import Header from "./components/Header.svelte";
-  import Theme from "./components/Theme.svelte";
 
   import AnkiConnectProvider from "./services/AnkiConnectProvider.svelte";
   import MappingsCreator from "./components/MappingsCreator.svelte";
   import SeachTab from "./components/SeachTab.svelte";
-  let theme: "g10" = "g10";
+
+  let isSideNavOpen = false;
 </script>
 
-<Theme bind:theme>
-  <Header />
-  <AnkiConnectProvider>
-    <Content>
-      <AnkiConectGate>
-        <Grid>
-          <Row>
-            <Column noGutter>
-              <Tabs aria-label="Tab navigation">
-                <Tab label="Search" />
-                <Tab label="Sources" />
-                <Tab label="Settings" />
-                <div slot="content" class="tabbed-content">
-                  <Grid as fullWidth>
-                    <TabContent>
-                      <SeachTab />
-                    </TabContent>
-                    <TabContent>
-                      <Row />
-                    </TabContent>
-                    <TabContent>
-                      <MappingsCreator />
-                    </TabContent>
-                  </Grid>
-                </div>
-              </Tabs>
-            </Column>
-          </Row>
-        </Grid>
-      </AnkiConectGate>
-    </Content>
-  </AnkiConnectProvider>
-</Theme>
+<Header company="Kyoro" platformName="Anki UI" bind:isSideNavOpen>
+  <HeaderNav>
+    <HeaderNavItem href="/" text="Start" />
+    <HeaderNavItem href="/" text="Account" />
+    <HeaderNavItem href="/" text="Local Storage" />
+    <HeaderNavMenu text="About">
+      <HeaderNavItem href="/" text="Kyoro" />
+      <HeaderNavItem href="/" text="Kyoro Team" />
+    </HeaderNavMenu>
+  </HeaderNav>
+</Header>
+
+<SideNav bind:isOpen={isSideNavOpen}>
+  <SideNavItems>
+    <SideNavLink text="New Cards" />
+    <SideNavLink text="Modify Cards" />
+    <SideNavDivider />
+    <SideNavLink text="Sentence Sources" />
+  </SideNavItems>
+</SideNav>
+
+<Content>
+  <Grid>
+    <Row>
+      <Column>
+        <AnkiConnectProvider>
+          <AnkiConectGate>
+            <Grid>
+              <Row>
+                <Column noGutter>
+                  <Tabs aria-label="Tab navigation">
+                    <Tab label="Search" />
+                    <Tab label="Sources" />
+                    <Tab label="Settings" />
+                    <div slot="content" class="tabbed-content">
+                      <Grid as fullWidth>
+                        <TabContent>
+                          <SeachTab />
+                        </TabContent>
+                        <TabContent>
+                          <Row />
+                        </TabContent>
+                        <TabContent>
+                          <MappingsCreator />
+                        </TabContent>
+                      </Grid>
+                    </div>
+                  </Tabs>
+                </Column>
+              </Row>
+            </Grid>
+          </AnkiConectGate>
+        </AnkiConnectProvider>
+      </Column>
+    </Row>
+  </Grid>
+</Content>
