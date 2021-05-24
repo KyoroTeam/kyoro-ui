e5a224247b37529be6c9401a15227e9e641cacc7
James <james.waugh.r@gmail.com>
2021-05-23 23:38:31 -0400

    Fleshing our CDK infrastrucutre more, adding in EB, VPC, EFS

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,10 +1,17 @@
 import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
 import * as s3 from "@aws-cdk/aws-s3";
+import * as efs from "@aws-cdk/aws-efs";
 import * as eb from "@aws-cdk/aws-elasticbeanstalk";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, `${id}-app-vpc`, {
+      enableDnsSupport: true,
+      enableDnsHostnames: true,
+    });
 
     const bucket = new s3.Bucket(this, `${id}-sources-bucket`, {});
 
@@ -17,19 +24,42 @@ export class InfrastructureStack extends cdk.Stack {
     });
 
     const appVersionProps = new eb.CfnApplicationVersion(this, "AppVersion", {
-      applicationName: "Tatoeba Sync",
+      applicationName: "MeiliSearch",
       sourceBundle: {
         s3Bucket: elbZipArchive.s3BucketName,
         s3Key: elbZipArchive.s3ObjectKey,
       },
     });
 
-    const elbEnv = new eb.CfnEnvironment(this, "Environment", {
-      environmentName: "dotnet-core-env",
-      applicationName: app.applicationName ?? "DotnetAppEnvName",
-      solutionStackName: "64bit Amazon Linux 2 v2.1.2 running .NET Core",
-      versionLabel: appVersionProps.ref,
+    const ebGroup = new ec2.SecurityGroup(this, "group", {
+      vpc: vpc,
     });
+
+    const elbEnv = new eb.CfnEnvironment(this, "Environment", {
+      environmentName: `${id}-meilisearch-environment`,
+      applicationName: "MeiliSearch",
+      solutionStackName: "64bit Amazon Linux 2 v3.3.0 running Docker",
+      versionLabel: appVersionProps.ref,
+      optionSettings: [
+        {
+          namespace: "aws:ec2:vpc",
+          optionName: "VPCId",
+          value: vpc.vpcId,
+        },
+        {
+          namespace: "aws:autoscaling:launchconfiguration",
+          optionName: "SecurityGroups",
+          value: ebGroup.securityGroupId,
+        },
+      ],
+    });
+
+    const filesystem = new efs.FileSystem(this, `${id}-meilisearch-fs`, {
+      vpc: vpc,
+      lifecyclePolicy: efs.LifecyclePolicy.AFTER_7_DAYS,
+    });
+
+    filesystem.connections.addSecurityGroup(ebGroup);
 
     // The code that defines your stack goes here
   }
