646377682f50d7bb9797a4b00f0386e84c85b319
James <james.waugh.r@gmail.com>
2021-07-20 21:44:36 -0400

    Basic mock Deno backend for dev testing, should port to Python

---
+++ mock-backend/README.md
@@ -0,0 +1,3 @@
+Development backend for mocking pycmd-based called to Kyoro outside of Anki.
+
+* Run `deno --unstable run --allow-net --allow-read server.ts`
---
+++ mock-backend/server.ts
@@ -0,0 +1,34 @@
+import { Application, Router } from "https://deno.land/x/oak@v8.0.0/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+
+const content = Deno.readTextFileSync("./all_v11_out.json");
+const SentenceData = JSON.parse(content) as any[];
+
+const router = new Router();
+
+console.log("Ready to Run");
+
+router
+  .get("/getLocalContentList", (context) => {
+    console.log("getLocalContentList");
+    const sources = SentenceData.map((d) => d.Source);
+    const uniqueSources = new Set(sources);
+    context.response.body = Array.from(uniqueSources.values());
+  })
+  .get("/getTokenizedSentences/:sourceId", (context) => {
+    const source = context.params.sourceId;
+    console.log("getTokenizedSentences", source);
+    const sources = SentenceData.filter((d) => d.Source === source);
+    context.response.body = sources;
+  });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use(
+  oakCors({
+    origin: true,
+  })
+);
+
+await app.listen({ port: 8006 });
