57327785fe02371411b80fbcf8a4f875057d307b
James <james.waugh.r@gmail.com>
2021-07-20 21:49:06 -0400

    Make CORS work

--- mock-backend/server.ts
+++ mock-backend/server.ts
@@ -23,12 +23,8 @@ router
   });
 
 const app = new Application();
+app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
-app.use(
-  oakCors({
-    origin: true,
-  })
-);
 
 await app.listen({ port: 8006 });
