f7afdd4fd6cc8bf4fcc2331b304cbb8548239686
James <james.waugh.r@gmail.com>
2022-02-12 09:30:07 -0500

    Remove more old files

--- app/src/models/KyoTokenResult.ts
+++
@@ -1,9 +0,0 @@
-export interface KyoTokenResult {
-  Source: string;
-  Sentence: string;
-  Translation?: string;
-  Words: string[];
-  WordPositions: { Start: number; End: number }[];
-  Lemmas: string[];
-  Readings: string[];
-}
--- app/src/models/SelectTableRow.ts
+++
@@ -1,9 +0,0 @@
-import type { SentecePart } from './Jibiki';
-
-export interface SelectTableRow {
-  selected: boolean;
-  sentenceParts: SentecePart[];
-  english?: string;
-  source: string;
-  tags: string[];
-}
